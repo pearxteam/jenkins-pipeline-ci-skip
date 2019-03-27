37bd80ed2bc5378ffea0dafa93aeea5574c453dc
mrAppleXZ <mr.applexz@gmail.com>
2019-03-27 22:27:37 +0500

    *a cat ran across my keyboard*

---
+++ vars/ciSkip.groovy
@@ -0,0 +1,28 @@
+def call(String step) {
+    if (step == 'check') {
+        check()
+    }
+    else if (step == 'postProcess') {
+        postProcess()
+    }
+    else {
+        error 'ciSkip has been called without valid `step` argument'
+    }
+}
+
+def check() {
+    env.CI_SKIP = "false"
+    if(currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause') == '[]') {
+        result = sh(script: "git log -1 | grep -i '.*\\[ci skip\\].*'", returnStatus: true)
+        if (result == 0) {
+            env.CI_SKIP = "true"
+            error "'[ci skip]' found in latest git commit message. Stopping the build."
+        }
+    }
+}
+
+def postProcess() {
+    if (env.CI_SKIP == "true") {
+        currentBuild.result = 'NOT_BUILT'
+    }
+}
