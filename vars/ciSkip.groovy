8a6fe66ce4ae573185e253c62611200c28ede32b
mrAppleXZ <mr.applexz@gmail.com>
2019-03-28 12:02:53 +0500

    Made parsing of getBuildResult() not so ugly
    Who debugs things using debug prints? Oh, it's me!
    
    Made the getBuildCauses() checking not so ugly
    
    Who didn't read the docs? Me!
    
    Removed the debug prints

--- vars/ciSkip.groovy
+++ vars/ciSkip.groovy
@@ -1,3 +1,5 @@
+import groovy.json.JsonSlurper
+
 def call(String step) {
     if (step == 'check') {
         check()
@@ -12,9 +14,8 @@ def call(String step) {
 
 def check() {
     env.CI_SKIP = "false"
-    if(currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause') == '[]') {
-        result = sh(script: "git log -1 | grep -i '.*\\[ci skip\\].*'", returnStatus: true)
-        if (result == 0) {
+    if(currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').isEmpty()) {
+        if (sh(script: "git log -1 | grep -i '.*\\[ci skip\\].*'", returnStatus: true) == 0) {
             env.CI_SKIP = "true"
             error "'[ci skip]' found in latest git commit message. Stopping the build."
         }
